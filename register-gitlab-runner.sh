7699dde1b8f0946a41e67e3158c11121c29084e8
Timur Kutzhanov <t.kutzhanov@aix.kz>
2019-10-18 13:47:22 +0600

    initial commit

---
+++ register-gitlab-runner.sh
@@ -0,0 +1,5 @@
+gitlab-runner register \
+      --url $GITLAB_URL --registration-token $REGISTRATION_TOKEN \
+      --executor docker --description "My Runner" \
+      --docker-image "docker:latest" \
+      --docker-volumes /var/run/docker.sock:/var/run/docker.sock
